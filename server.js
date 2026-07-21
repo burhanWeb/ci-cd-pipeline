231bb90a71b5b49f602cf20eb291a354de1cc07f
burhansidiqWeb <burhan.radah@voctrum.com>
2026-07-21 07:06:47 +0530

    new changes

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from 'express'
 
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello Wccdcdccorld!'))
 
 app.listen(3000,()=>{
     console.log('Server is ru  vnning on port 3000')
