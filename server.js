78dba57dcc018856d29360396006263258eb1cff
burhansidiqWeb <burhan.radah@voctrum.com>
2026-07-21 08:07:13 +0530

    feat : add new text

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from 'express'
 
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello Wccdcdccorld!'))
+app.get('/', (req, res) => res.send('coming......'))
 
 app.listen(3000,()=>{
     console.log('Server is ru  vnning on port 3000')
